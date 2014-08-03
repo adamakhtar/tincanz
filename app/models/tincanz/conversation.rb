1ae194803c8c6a8f727ef2935b641aa83e2108e8
robodisco <adamsubscribe@googlemail.com>
2014-08-03 17:46:14 +0900

    create #participants for conversaton

--- app/models/tincanz/conversation.rb
+++ app/models/tincanz/conversation.rb
@@ -5,22 +5,42 @@ module Tincanz
 
     has_many :receipts, 
              through: :messages
+
+    has_many :recipients,
+             -> { uniq },
+             through: :receipts,
+             class_name: Tincanz.user_class
     
+    has_many :authors,
+             -> { uniq },
+             through: :messages,
+             source: :user,
+             class_name: Tincanz.user_class
+
     has_one  :first_message, 
              -> { order('tincanz_messages.created_at ASC').limit(1) },
              class_name: 'Tincanz::Message'
 
     has_many :subsequent_messages,
-              -> { order('tincanz_messages.updated_at ASC').offset(1) },
-              class_name: 'Tincanz::Message'         
+              -> { order('tincanz_messages.created_at ASC').offset(1) },
+             class_name: 'Tincanz::Message'              
+
+    has_one  :most_recent_message,
+             -> { order('tincanz_messages.created_at DESC').limit(1) },
+             class_name: 'Tincanz::Message'         
+
 
     scope :involving, -> (user) { joins(messages: :receipts).where(["tincanz_messages.user_id = :user_id OR tincanz_receipts.recipient_id = :user_id", user_id: user.id]) }
 
-    scope :recent, -> { order('updated_at DESC') }
+    scope :recent, -> { order('created_at DESC') }
 
     accepts_nested_attributes_for :messages
 
     belongs_to :user, class_name: Tincanz.user_class
+
+    def participants
+      (authors + recipients).uniq
+    end
   end
 end
 
